8480be8150f8fde9a4be016bb48e69fff49b4f6f
mahfujalam1 <mahfujahmad424@gmail.com>
2026-01-28 12:25:47 +0600

    u

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-
 import Login from "../Pages/auth/Login";
 import ForgetPassword from "../Pages/auth/ForgetPassword";
 import VerifyCode from "../Pages/auth/VerifyCode";
 import SetNewPassword from "../Pages/auth/SetNewPassword";
-
 import AdminRoute from "../ProtectedRoute/AdminRoute";
 import Dashboard from "../Pages/layout/Dashboard";
 import PrivacyPolicy from "../Pages/privacyPolicy/PrivacyPolicy";
